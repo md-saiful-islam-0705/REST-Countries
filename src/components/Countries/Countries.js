e1c7c3ce8aae9844c874a4ae3d8bea755ef16ad4
saifulislam2711 <saiful111066@gmail.com>
2022-03-26 08:46:13 +0200

    api modified

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
+import './Countries.css';
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect( ()=>{
+        
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data));
+
+    }, [])
+
+    return (
+        <div>
+            <h1>Let's have a world tour!!</h1>
+            <h2>Countries: {countries.length}</h2>
+            <div className='countries-container'>
+            {
+                countries.map(country => <Country 
+                    country={country}
+                    key={country.cca3}
+                    ></Country>)
+            }
+            </div>
+        </div>
+    );
+};
+
+export default Countries;
